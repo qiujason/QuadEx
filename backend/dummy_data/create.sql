4396166e699c103bb3e0ce64f1662eb7b2d01c9a
Andrew Lee <andrew.j.lee@duke.edu>
2021-10-21 00:22:37 -0400

    tables establishing

--- backend/dummy_data/create.sql
+++ backend/dummy_data/create.sql
@@ -8,32 +8,59 @@ CREATE TABLE users(
     password VARCHAR(30) NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    birthday TIMESTAMP NOT NULL,
     year INTEGER NOT NULL,
+    hometown TEXT NOT NULL,
     quad VARCHAR(30) REFERENCES quad(name),
-    birthday TIMESTAMP,
     degree TEXT,
-    hometown TEXT,
-    bio TEXT
+    bio TEXT,
+    insta TEXT,
+    bday_cal BOOLEAN DEFAULT TRUE NOT NULL
 );
+
+CREATE TABLE admin(
+    username VARCHAR(8) PRIMARY KEY REFERENCES users(netID),
+    title VARCHAR(50) NOT NULL,
+    email VARCHAR(50),
+)
 
 CREATE TABLE points(
     id SERIAL PRIMARY KEY,
     netID VARCHAR(8) references users(netID),
-    time_stamp TIMESTAMP,
-    point_value INTEGER,
-    reason TEXT
+    time_stamp TIMESTAMP NOT NULL,
+    point_value INTEGER NOT NULL,
+    reason TEXT NOT NULL
 );
 
 CREATE TABLE events(
     id SERIAL PRIMARY KEY,
     title VARCHAR(150) NOT NULL,
+    time TIMESTAMP NOT NULL,
     description TEXT,
     location VARCHAR(30),
-    time TIMESTAMP
+    tags VARCHAR(30) []
 );
 
-CREATE TABLE event_interest(
+CREATE TABLE favorited_events(
     netID VARCHAR(8) references users(netID),
     event_id SERIAL references events(id),
     PRIMARY KEY(netID, event_id)
+);
+
+CREATE TABLE quad_events(
+    quad_name VARCHAR(30) references quads(name),
+    event_id SERIAL references events(id),
+    PRIMARY KEY(quad_name, event_id)
+);
+
+CREATE TABLE user_member_of(
+    netID VARCHAR(8) references users(netID),
+    quad_name VARCHAR(30) references quads(name),
+    PRIMARY KEY(netID, quad_name)
+);
+
+CREATE TABLE points_earned_by(
+    netID VARCHAR(8) references users(netID),
+    points_id SERIAL references points(id),
+    PRIMARY KEY(netID, points_id)
 );
