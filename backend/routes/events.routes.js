0a848f9d8925aba0b36dea7ca97ce580fd1bd02f
Jason Qiu <jason.qiu@duke.edu>
2021-11-07 14:16:36 -0500

    add endpoints for favorite events

--- backend/routes/events.routes.js
+++ backend/routes/events.routes.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const eventsControllers = require('../controllers/events.controllers')
+const favoriteEventsControllers = require('../controllers/favoriteEvents.controllers')
 const router = express.Router();
 
 router.get('/', eventsControllers.getEvents)
 router.post('/', eventsControllers.postEvents)
 router.put('/', eventsControllers.putEvents)
 router.delete('/', eventsControllers.deleteEvents)
+
+router.get('/favoriteByUser', favoriteEventsControllers.getFavoriteEventsByUser)
+router.get('/listUsers', favoriteEventsControllers.getUsersByFavoriteEvents)
+router.post('/favoriteForUser', favoriteEventsControllers.postFavoriteEvents)
+router.delete('/favoriteForUser', favoriteEventsControllers.deleteFavoriteEvents)
 
 module.exports = router
