994c462340859f7e2c5f9c7c1c12d17fd29f63e3
Donghan Park <donghan.park@duke.edu>
2021-11-08 23:15:19 -0500

    added update event tags to events page

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -11,8 +11,7 @@ const Events = () => {
     const [ showPastEvents, setShowPastEvents ] = useState(false);
 
     async function fetchEvents() {
-        const net_id = 'dp239';
-        let eventsResponse = await fetch('http://localhost:3001/events/favoriteByUser/?id=' + net_id);
+        let eventsResponse = await fetch('http://localhost:3001/events');
         let eventsData = await eventsResponse.json();
         console.log(eventsData);
         setAllEvents(eventsData);
@@ -28,6 +27,28 @@ const Events = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [allEvents]);
 
+    useEffect(() => {
+        if(showPastEvents){
+            setRenderedEvents(allEvents);
+            return;
+        } else {
+            setRenderedEvents([]);
+            const date = new Date();
+            var currDate, eventDate;
+            allEvents.forEach(eventObj => {
+                currDate = Number(String(date.getFullYear()) + String(date.getMonth() + 1).padStart(2, '0') + String(date.getDate()).padStart(2, '0') + String(date.getHours()).padStart(2, '0') + String(date.getMinutes()).padStart(2, '0'));
+                eventDate = Number(eventObj.date.substring(4) + eventObj.date.substring(0, 2) + eventObj.date.substring(2, 4) + eventObj.time.substring(0, 2) + eventObj.time.substring(2));
+                if(eventDate >= currDate) setRenderedEvents(renderedEvents => [...renderedEvents, eventObj]);
+            });
+        }
+    }, [showPastEvents, allEvents]);
+
+    // sort events list chronologically
+    renderedEvents.sort(function (i1, i2){
+        var year1 = Number(i1.date.substring(4) + i1.date.substring(0, 2) + i1.date.substring(2, 4));
+        var year2 = Number(i2.date.substring(4) + i2.date.substring(0, 2) + i2.date.substring(2, 4));
+        return year1 - year2;
+    });
 
     // filter rendered events by title
     const filterTitle = (str) => {
