efefaef043a8327a3d6940da9ded77f554dbc161
Donghan Park <donghan.park@duke.edu>
2021-11-23 23:30:09 -0500

    added next, prev, and today buttons for stepping weeks on calendar

--- frontend/src/pages/Quad.js
+++ frontend/src/pages/Quad.js
@@ -4,18 +4,26 @@ import '../stylesheets/QuadPage.scss'
 
 const Quad = () => {
     const [ columns, setColumns ] = useState([]);
+    const [ weekIncrement, setWeekIncrement ] = useState(0);
     const dayNames = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
 
     useEffect(() => {
         updateColumns();
-    }, []);
+    }, [weekIncrement]);
+
+    const changeWeek = (increment) => {
+        setWeekIncrement(weekIncrement + increment);
+    }
+
+    const resetWeek = () => {
+        setWeekIncrement(0);
+    }
 
     const updateColumns = () => {
         var newColumns = [];
         for(let i = 0; i < 7; i++){
             const date = new Date();
-            const currFirst = date.getDate() - date.getDay() + 7*14;
-            console.log(currFirst);
+            const currFirst = date.getDate() - date.getDay() + 7 * weekIncrement;
             const newDate = new Date(date.setDate(currFirst + i));
             const newFirst = newDate.getDate();
             const newMonth = newDate.getMonth() + 1;
@@ -35,6 +43,10 @@ const Quad = () => {
             <div className="calendar-container">
                 <div className="title-container">
                     <h1>TITLE</h1>
+                    <p className='prev-btn' onClick={() => changeWeek(-1)}>prev</p>
+                    <p className='next-btn' onClick={() => changeWeek(1)}>next</p>
+                    
+                    <p className='today-btn' onClick={() => resetWeek()}>today</p>
                 </div>
                 {columns.length > 0 ?
                     columns
